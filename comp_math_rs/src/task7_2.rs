684efc8b92a6ef28e00d90bf5d8b3b05029901cf
vlad-cool <vlad-matveev2004@mail.ru>
2025-05-09 17:57:39 +0300

    sturm-liuville problem works

--- comp_math_rs/src/task7_2.rs
+++ comp_math_rs/src/task7_2.rs
@@ -20,17 +20,19 @@ mod utils;
 
 fn write_csv<const N: usize>(
     group: String,
+    path: String,
     solution: CauchySolution<N>,
     tau: f64,
+    self_value: f64,
     time: std::time::Duration,
 ) {
     let mut file =
-        std::fs::File::create(format!("../comp_math_rs/test.csv")).expect("Failed to open file");
+        std::fs::File::create(format!("../task7_2_data/{path}.csv")).expect("Failed to open file");
     file.write(format!("{}\n", group).as_bytes())
         .expect("failed to wrtite to file");
-    file.write(format!("{}\n", solution.method_name).as_bytes())
-        .expect("failed to write to file");
     file.write(format!("{}\n", tau).as_bytes())
+        .expect("failed to write to file");
+    file.write(format!("{}\n", self_value).as_bytes())
         .expect("failed to write to file");
     file.write(format!("{}\n", time.as_secs_f64()).as_bytes())
         .expect("failed to write to file");
@@ -54,23 +56,6 @@ fn main() {
             "Kutta's third-order method (Explicit)".to_string(),
         );
 
-    let equation = |v: &[f64; 1]| {
-        let v: f64 = v[0];
-        let a: f64 = v;
-        let a: f64 = v.exp() + 10.0;
-
-        let mut cauchy_problem: CauchyProblem<2, _> = CauchyProblem {
-            f: &mut |_: f64, x: &[f64; 2]| [a * x[1], (1.0 - x[0].exp()) / a],
-            start: 0.0,
-            stop: 120.0,
-            x_0: [0.0, 1.0],
-        };
-        let (solution, res) = cauchy_solver.solve(&mut cauchy_problem, 0.001, false, None);
-        res.expect("Faield to solve differential equation");
-        [solution.x.last().unwrap()[0].powi(2) * 1.0
-            + (solution.x.last().unwrap()[1] - 1.0).powi(2) * 1.0]
-    };
-
     // let solver: backward_differentiation_method::BackwardDifferentiationMethod<2> =
     //     backward_differentiation_method::BackwardDifferentiationMethod::new(
     //         1,
@@ -81,26 +66,56 @@ fn main() {
     // let (solution, _res) =
     //     crate::shooting_method::shooting_method(&mut problem, &mut solver, 0.001);
     // let duration: std::time::Duration = start_time.elapsed();
-    let res = solve_newton(equation, &[0.0], None);
-    let a: f64 = res.unwrap()[0];
-    let a: f64 = a.exp() + 10.0;
 
-    println!("a: {a}");
+    for i in 5..30 {
+        let i = i * 2;
+        let start_time: std::time::Instant = std::time::Instant::now();
+        let equation = |v: &[f64; 1]| {
+            let v: f64 = v[0];
+            let a: f64 = v;
+            // let a: f64 = a.exp() + 0.0;
 
-    let mut cauchy_problem: CauchyProblem<2, _> = CauchyProblem {
-        f: &mut |_: f64, x: &[f64; 2]| [x[1], 1.0 - x[0].exp()],
-        start: 0.0,
-        stop: 120.0,
-        x_0: [0.0, a],
-    };
+            let mut cauchy_problem: CauchyProblem<2, _> = CauchyProblem {
+                f: &mut |_: f64, x: &[f64; 2]| [a * x[1], (1.0 - x[0].exp()) / a],
+                start: 0.0,
+                stop: 120.0,
+                x_0: [0.0, 1.0],
+            };
+            let (solution, res) = cauchy_solver.solve(&mut cauchy_problem, 0.01, false, None);
+            res.expect("Faield to solve differential equation");
+            [(solution.x.last().unwrap()[0].powi(2) * 10.0
+                + (solution.x.last().unwrap()[1] - 1.0).powi(2) * 1.0)]
+        };
 
-    let (solution, _res) = cauchy_solver.solve(&mut cauchy_problem, 0.001, false, None);
-    println!("{:?}", solution.x.last().unwrap());
+        let res: Result<[f64; 1], &'static str> = solve_newton(equation, &[i as f64], None);
+        
+        let a = match res {
+            Err(_) => continue,
+            Ok(val) => val,
+        };
+        
+        let a: f64 = a[0];
+        // let a: f64 = a.exp() + 10.0;
 
-    write_csv(
-        format!("shooting_method"),
-        solution,
-        0.001,
-        std::time::Duration::from_micros(0),
-    );
+        // println!("a: {a}");
+
+        let mut cauchy_problem: CauchyProblem<2, _> = CauchyProblem {
+            f: &mut |_: f64, x: &[f64; 2]| [a * x[1], (1.0 - x[0].exp()) / a],
+            start: 0.0,
+            stop: 120.0,
+            x_0: [0.0, 1.0],
+        };
+
+        let (solution, _res) = cauchy_solver.solve(&mut cauchy_problem, 0.001, false, None);
+        // println!("{:?}", solution.x.last().unwrap());
+
+        write_csv(
+            format!("shooting_method"),
+            format!("{i}"),
+            solution,
+            0.001,
+            a,
+            start_time.elapsed(),
+        );
+    }
 }
